052b79bfcbd4004b5221d4432f75bcd363cf1d16
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-09-15 22:04:06 -0400

    first commit

---
+++ Bit-Source/src/components/SiteNavigation.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="sticky top-0 bg-main-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"></nav>
+    </header>
+</template>
+
+<script setup></script>
+
